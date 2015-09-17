98c8dd46e2e634db88cf2f311d1e82418eed9f27
allenwtl <415522571@qq.com>
2015-09-17 14:07:44 +0800

    修改测试

--- spring/spring-beans/src/main/java/org/allen/springbeans/TestSpringBeans.java
+++ spring/spring-beans/src/main/java/org/allen/springbeans/TestSpringBeans.java
@@ -2,26 +2,46 @@ package org.allen.springbeans;
 
 import org.junit.Before;
 import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class TestSpringBeans {
 	
+	
+	private String youName ;
+	
 	private ApplicationContext applicationContext ;
 	
 	@Before
 	public void prepare(){
-		applicationContext = new ClassPathXmlApplicationContext("classpath*:/spring-config/spring-context.xml");
+		applicationContext = new ClassPathXmlApplicationContext("classpath:spring-config/spring-context.xml");
 		
 	}
 	
 	@Test
 	public void test(){
+		
 		String contextName = applicationContext.getApplicationName();
 		String displayName = applicationContext.getDisplayName();
 		System.out.println("contextName:"+contextName);
 		System.out.println("displayName:"+displayName);
+		
+		String [] beanDeCount = applicationContext.getBeanDefinitionNames();
+		for (String string : beanDeCount) {
+			Object beanDe = applicationContext.getBean(string, Object.class);
+			System.out.println(string+":"+ beanDe);
+			
+		}
+	}
+
+	public String getYouName() {
+		return youName;
+	}
+
+	public void setYouName(String youName) {
+		this.youName = youName;
 	}
 	
-	
+	 
 }
